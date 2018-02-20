abfcb556ffc0497644b7ccbd5cfc60cf40039f15
timmythetiny <timharding@me.com>
2018-02-19 21:46:50 -0800

    Case on voxel algorithm working
    Not building right now. Just want to make this commit before switching to GetIndexAtPosition format, rather than setting up vertices for an entire voxel at a time.  Realizing that other than case 1, not all generated vertices on a voxel may be used by triangles.

--- Assets/Sandbox/VoxelGrid.cs
+++ Assets/Sandbox/VoxelGrid.cs
@@ -2,31 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+internal struct Voxel
+{
+    public bool Mode;
+    public Vector2 Position;
+
+    // Indices into vertex list, once they have been used
+    public int Base;
+    public int YEdge;
+    public int XEdge;
+
+    public Voxel(Vector2 position, bool mode)
+    {
+        Mode = mode;
+        Position = position;
+        Base = -1;
+        YEdge = -1;
+        XEdge = -1;
+    }
+}
+
 public class VoxelGrid : MonoBehaviour {
 
     public int XResolution = 16;
     public int YResolution = 16;
     
-    private bool[] Cells;
+    private Voxel[] Cells;
     private bool NeedsRedraw = true;
     private MeshFilter filter;
 
     private List<Vector3> vertices = new List<Vector3>();
     private List<Vector3> normals = new List<Vector3>();
-    private List<int> trianglesOn = new List<int>();
-    private List<int> trianglesOff = new List<int>();
+    private List<int> triangles = new List<int>();
 
     void Start()
     {
         filter = GetComponent<MeshFilter>();
-        Cells = new bool[XResolution * YResolution];
+
+        int cellsSize = XResolution * YResolution;
+        Cells = new Voxel[cellsSize];
+        for (int cell = 0; cell < cellsSize; cell++)
+        {
+            Cells[cell] = new Voxel();
+        }
     }
 
     void Update()
     {
         if (NeedsRedraw)
         {
-            RefreshGrid();
+            Triangulate();
+            NeedsRedraw = false;
         }
     }
 
@@ -48,10 +74,11 @@ public class VoxelGrid : MonoBehaviour {
                 float distance = xDifference * xDifference + yDifference * yDifference;
                 if (distance < size * size)
                 {
+                    // Don't use GetCell because this is the only place I really need index bounds checking
                     int index = y * XResolution + x;
                     if (index < Cells.Length && index > 0)
                     {
-                        Cells[index] = mode;
+                        Cells[index].Mode = mode;
                     }
                 }
             }
@@ -59,60 +86,99 @@ public class VoxelGrid : MonoBehaviour {
         NeedsRedraw = true;
     }
 
-    private void RefreshGrid()
+    private void Triangulate()
     {
-        if (filter == null)
-        {
-            Debug.Log("Filter is null.");
-            return;
-        }
-
         Mesh mesh = new Mesh();
-        mesh.name = "Voxel Preview Mesh";
-        mesh.subMeshCount = 2;
+        mesh.name = "Marching Squares";
+
+        for (int cell = 0; cell < Cells.Length; cell++)
+        {
+            bool mode = Cells[cell].Mode;
+            Vector2 position = new Vector2(
+                cell % XResolution - XResolution / 2,
+                cell / XResolution - YResolution / 2
+            );
+            Voxel voxel = new Voxel(position, mode);
+            Cells[cell] = voxel;
+        }
 
         vertices.Clear();
         normals.Clear();
-        trianglesOn.Clear();
-        trianglesOff.Clear();
+        triangles.Clear();
 
-        for (int x = 0; x < XResolution; x++)
+        for (int x = 0; x < XResolution - 1; x++)
         {
-            for (int y = 0; y < YResolution; y++)
+            for (int y = 0; y < YResolution - 1; y++)
             {
-                MakeQuad(new Vector3(x - XResolution / 2, y - YResolution / 2, 0), 0.2f, Cells[y * XResolution + x]);
+                Voxel lowerLeft = GetCell(x, y);
+                Voxel lowerRight = GetCell(x + 1, y);
+                Voxel upperLeft = GetCell(x, y + 1);
+                Voxel upperRight = GetCell(x + 1, y + 1);
+                
+                int cellType = lowerLeft.Mode ? 1 : 0;
+                cellType |= lowerRight.Mode ? 2 : 0;
+                cellType |= upperLeft.Mode ? 4 : 0;
+                cellType |= upperRight.Mode ? 8 : 0;
+
+                switch (cellType)
+                {
+                    case 0:
+                        break;
+                    default:
+                        SetUpVoxels(lowerLeft);
+                        Triangle(lowerLeft.Base, lowerLeft.YEdge, lowerLeft.XEdge);
+                        break;
+                    //case 2:
+                    //    SetUpVoxels(lowerLeft, lowerRight);
+                    //    Triangle(lowerLeft.XEdge, lowerRight.YEdge, lowerRight.Base);
+                    //    break;
+                }
             }
         }
-        
-        mesh.vertices = vertices.ToArray();
-        mesh.normals = normals.ToArray();
-        mesh.SetTriangles(trianglesOn, 0);
-        mesh.SetTriangles(trianglesOff, 1);
-        filter.mesh = mesh;
 
-        NeedsRedraw = false;
+        mesh.vertices = vertices.ToArray();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            normals.Add(Vector3.forward);
+        }
+        mesh.normals = normals.ToArray();
+        mesh.triangles = triangles.ToArray();
+        filter.mesh = mesh;
     }
 
-    private void MakeQuad(Vector3 center, float size, bool mode)
+    private void Triangle(int first, int second, int third)
     {
-        vertices.Add(center + new Vector3(-size, -size, 0));
-        vertices.Add(center + new Vector3(-size, size, 0));
-        vertices.Add(center + new Vector3(size, size, 0));
-        vertices.Add(center + new Vector3(size, -size, 0));
+        triangles.Add(first);
+        triangles.Add(second);
+        triangles.Add(third);
+    }
 
-        for (int i = 0; i < 4; i++)
+    private void SetUpVoxels(params Voxel[] voxels)
+    {
+        foreach (Voxel voxel in voxels)
         {
-            normals.Add(-Vector3.forward);
+            SetUpVoxel(voxel);
         }
+    }
 
-        List<int> triangles = mode ? trianglesOn : trianglesOff;
+    private void SetUpVoxel(Voxel voxel)
+    {
+        SetUpVoxelIndex(ref voxel.Base, voxel.Position);
+        SetUpVoxelIndex(ref voxel.XEdge, voxel.Position + new Vector2(0.5f, 0f));
+        SetUpVoxelIndex(ref voxel.YEdge, voxel.Position + new Vector2(0f, 0.5f));
+    }
 
-        int count = vertices.Count;
-        triangles.Add(count - 4);
-        triangles.Add(count - 3);
-        triangles.Add(count - 1);
-        triangles.Add(count - 3);
-        triangles.Add(count - 2);
-        triangles.Add(count - 1);
+    private void SetUpVoxelIndex(ref int index, Vector2 position)
+    {
+        if (index == -1)
+        {
+            index = vertices.Count;
+            vertices.Add(new Vector3(position.x - XResolution / 2, position.y - YResolution / 2, 0));
+        }
+    }
+
+    private Voxel GetCell(int x, int y)
+    {
+        return Cells[y * XResolution + x];
     }
 }
