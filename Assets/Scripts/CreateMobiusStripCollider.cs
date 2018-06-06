f2dedea0a0827be5ca9ae281785b74230ac1dd6a
timmythetiny <account@timharding.co>
2018-06-05 22:06:29 -0700

    Mobius procedural working
    Want to get the normals to wrap nicer, set up UVs, fix a couple issues.

---
+++ Assets/Scripts/CreateMobiusStripCollider.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class CreateMobiusStripCollider : MonoBehaviour {
+
+    public float MidcircleRadius = 1.0f;
+    public float HalfWidth = 0.25f;
+    public int Divisions = 256;
+
+    void Start()
+    {
+        CreateMesh();
+    }
+
+    void OnValidate()
+    {
+        CreateMesh();
+    }
+
+    /// <summary>
+    /// http://mathworld.wolfram.com/MoebiusStrip.html
+    /// </summary>
+    private void CreateMesh()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[Divisions * 2];
+        int[] triangles = new int[(Divisions * 2) * 3];
+        Vector3[] normals = new Vector3[Divisions * 2];
+
+        int vertsMod = Divisions * 2 - 1;
+
+        for (int u = 0; u < Divisions; u++)
+        {
+            for (int v = 0; v < 2; v++)
+            {
+                float s = v == 0 ? -HalfWidth : HalfWidth;
+                float t = (float)u / Divisions * Mathf.PI * 2.0f;
+                float radial = MidcircleRadius + s * Mathf.Cos(0.5f * t);
+                float x = radial * Mathf.Cos(t);
+                float z = radial * Mathf.Sin(t);
+                float y = s * Mathf.Sin(0.5f * t);
+
+                int vert = (u * 2 + v);
+                vertices[vert] = new Vector3(x, y, z);
+
+                triangles[vert * 3] = (vert + v * 2) % vertsMod;
+                triangles[vert * 3 + 1] = (vert + 1) % vertsMod;
+                triangles[vert * 3 + 2] = (vert + 2 - v * 2) % vertsMod;
+            }
+        }
+
+        for (int i = 0; i < Divisions; i++)
+        {
+            int vert = i * 2;
+            Vector3 current = vertices[vert];
+            Vector3 v1 = vertices[vert + 1] - current;
+            Vector3 v2 = vertices[(vert + 2) % vertsMod] - current;
+            Vector3 normal = -Vector3.Cross(v1.normalized, v2.normalized);
+            normals[vert] = normal;
+            normals[vert + 1] = normal;
+        }
+
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.triangles = triangles;
+        mesh.name = "Mobius Procedural";
+        filter.mesh = mesh;
+    }
+}
---
+++ Assets/Shaders/MobiusTestingShader.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/MobiusTestingShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		Cull Off
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+			float3 viewDir;
+			float3 worldNormal; INTERNAL_DATA
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			fixed4 color = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = color.rgb;
+			o.Alpha = color.a;
+
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+
+			o.Normal = dot(IN.viewDir, float3(0, 0, 1)) > 0 ? float3(0, 0, 1) : float3(0, 0, -1); 
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
