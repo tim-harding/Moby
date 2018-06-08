15e6839d27c5570e5cbb0fcee5cd765b52cad001
timmythetiny <timharding@me.com>
2018-06-07 20:32:08 -0700

    Progress.
    I did this a long time ago and never commited, not sure of the details.

--- Assets/Sandbox/VoxelGrid.cs
+++ Assets/Sandbox/VoxelGrid.cs
@@ -2,32 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-internal struct Voxel
-{
-    public bool Mode;
-    public Vector2 Position;
-
-    // Indices into vertex list, once they have been used
-    public int Base;
-    public int YEdge;
-    public int XEdge;
-
-    public Voxel(Vector2 position, bool mode)
-    {
-        Mode = mode;
-        Position = position;
-        Base = -1;
-        YEdge = -1;
-        XEdge = -1;
-    }
-}
-
 public class VoxelGrid : MonoBehaviour {
 
     public int XResolution = 16;
     public int YResolution = 16;
     
-    private Voxel[] Cells;
+    private Voxel[,] Cells;
     private bool NeedsRedraw = true;
     private MeshFilter filter;
 
@@ -38,12 +18,14 @@ public class VoxelGrid : MonoBehaviour {
     void Start()
     {
         filter = GetComponent<MeshFilter>();
-
-        int cellsSize = XResolution * YResolution;
-        Cells = new Voxel[cellsSize];
-        for (int cell = 0; cell < cellsSize; cell++)
+        
+        Cells = new Voxel[XResolution, YResolution];
+        for (int x = 0; x < XResolution; x++)
         {
-            Cells[cell] = new Voxel();
+            for (int y = 0; y < YResolution; y++)
+            {
+                Cells[x, y] = new Voxel(false);
+            }
         }
     }
 
@@ -58,6 +40,8 @@ public class VoxelGrid : MonoBehaviour {
 
     public void PaintCircle(Vector2 center, float size, bool mode)
     {
+        Vector2Int halfRes = new Vector2Int(XResolution / 2, YResolution / 2);
+        float squareSize = size * size;
         center += new Vector2(XResolution / 2, YResolution / 2);
         for (int y = (int)(center.y - size),
             yMax = (int)(center.y + size + 1);
@@ -69,16 +53,14 @@ public class VoxelGrid : MonoBehaviour {
                 x < xMax;
                 x++)
             {
-                float xDifference = Mathf.Abs(center.x - x);
-                float yDifference = Mathf.Abs(center.y - y);
-                float distance = xDifference * xDifference + yDifference * yDifference;
-                if (distance < size * size)
+                if (Mathf.Abs(x) < halfRes.x && Mathf.Abs(y) < halfRes.y)
                 {
-                    // Don't use GetCell because this is the only place I really need index bounds checking
-                    int index = y * XResolution + x;
-                    if (index < Cells.Length && index > 0)
+                    float xDifference = Mathf.Abs(center.x - x);
+                    float yDifference = Mathf.Abs(center.y - y);
+                    float distance = xDifference * xDifference + yDifference * yDifference;
+                    if (distance < squareSize)
                     {
-                        Cells[index].Mode = mode;
+                        Cells[x + halfRes.x, y + halfRes.y].Mode = mode;
                     }
                 }
             }
@@ -91,15 +73,12 @@ public class VoxelGrid : MonoBehaviour {
         Mesh mesh = new Mesh();
         mesh.name = "Marching Squares";
 
-        for (int cell = 0; cell < Cells.Length; cell++)
+        for (int x = 0; x < XResolution; x++)
         {
-            bool mode = Cells[cell].Mode;
-            Vector2 position = new Vector2(
-                cell % XResolution - XResolution / 2,
-                cell / XResolution - YResolution / 2
-            );
-            Voxel voxel = new Voxel(position, mode);
-            Cells[cell] = voxel;
+            for (int y = 0; y < YResolution; y++)
+            {
+                Cells[x, y].ResetIndices();
+            }
         }
 
         vertices.Clear();
@@ -110,10 +89,10 @@ public class VoxelGrid : MonoBehaviour {
         {
             for (int y = 0; y < YResolution - 1; y++)
             {
-                Voxel lowerLeft = GetCell(x, y);
-                Voxel lowerRight = GetCell(x + 1, y);
-                Voxel upperLeft = GetCell(x, y + 1);
-                Voxel upperRight = GetCell(x + 1, y + 1);
+                Voxel lowerLeft = Cells[x, y];
+                Voxel lowerRight = Cells[x + 1, y];
+                Voxel upperLeft = Cells[x, y + 1];
+                Voxel upperRight = Cells[x + 1, y + 1];
                 
                 int cellType = lowerLeft.Mode ? 1 : 0;
                 cellType |= lowerRight.Mode ? 2 : 0;
@@ -125,60 +104,31 @@ public class VoxelGrid : MonoBehaviour {
                     case 0:
                         break;
                     default:
-                        SetUpVoxels(lowerLeft);
-                        Triangle(lowerLeft.Base, lowerLeft.YEdge, lowerLeft.XEdge);
+                        AddVert(x, y, 0, ref lowerLeft.Base);
+                        AddVert(x, y + 0.5f, 0, ref lowerLeft.YEdge);
+                        AddVert(x + 0.5f, y, 0, ref lowerLeft.XEdge);
                         break;
-                    //case 2:
-                    //    SetUpVoxels(lowerLeft, lowerRight);
-                    //    Triangle(lowerLeft.XEdge, lowerRight.YEdge, lowerRight.Base);
-                    //    break;
                 }
             }
         }
 
-        mesh.vertices = vertices.ToArray();
         for (int i = 0; i < vertices.Count; i++)
         {
             normals.Add(Vector3.forward);
         }
+        mesh.vertices = vertices.ToArray();
         mesh.normals = normals.ToArray();
         mesh.triangles = triangles.ToArray();
         filter.mesh = mesh;
     }
 
-    private void Triangle(int first, int second, int third)
-    {
-        triangles.Add(first);
-        triangles.Add(second);
-        triangles.Add(third);
-    }
-
-    private void SetUpVoxels(params Voxel[] voxels)
-    {
-        foreach (Voxel voxel in voxels)
-        {
-            SetUpVoxel(voxel);
-        }
-    }
-
-    private void SetUpVoxel(Voxel voxel)
-    {
-        SetUpVoxelIndex(ref voxel.Base, voxel.Position);
-        SetUpVoxelIndex(ref voxel.XEdge, voxel.Position + new Vector2(0.5f, 0f));
-        SetUpVoxelIndex(ref voxel.YEdge, voxel.Position + new Vector2(0f, 0.5f));
-    }
-
-    private void SetUpVoxelIndex(ref int index, Vector2 position)
+    private void AddVert(float x, float y, float z, ref int index)
     {
         if (index == -1)
         {
             index = vertices.Count;
-            vertices.Add(new Vector3(position.x - XResolution / 2, position.y - YResolution / 2, 0));
+            vertices.Add(new Vector3(x - XResolution / 2, y - YResolution / 2, z));
         }
-    }
-
-    private Voxel GetCell(int x, int y)
-    {
-        return Cells[y * XResolution + x];
+        triangles.Add(index);
     }
 }
